4e3cebde9504b4e0b9a820139d8068998e2e3cfa
Neil Desai <neil@elastiflow.com>
2025-07-28 13:44:00 -0500

    Add "metric" field support for object attributes
    This commit introduces a new "metric" field for object attributes, allowing enumeration of "gauge" and "counter" values. It updates schema definitions, object logic, and tests to validate the new field and handle potential errors during validation. This enhancement ensures improved metric handling within the object schema.

--- pkg/def/object.go
+++ pkg/def/object.go
@@ -33,6 +33,8 @@ type ObjectAttribute struct {
 	Tag        bool                    `yaml:"tag,omitempty" json:"tag,omitempty"`
 	Name       string                  `yaml:"name,omitempty" json:"name,omitempty"`
 	Syntax     string                  `yaml:"syntax,omitempty" json:"syntax,omitempty"`
+	Semantics  string                  `yaml:"semantics,omitempty" json:"semantics,omitempty"`
+	Metric     string                  `yaml:"metric,omitempty" json:"metric,omitempty"`
 	Rediscover string                  `yaml:"rediscover,omitempty" json:"rediscover,omitempty"`
 	Overrides  ObjectAttributeOverride `yaml:"overrides,omitempty" json:"overrides,omitempty"`
 }
